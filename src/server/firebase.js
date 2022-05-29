49523c6ae6d796c7ffaef24bc2c914ce1baa414a
Md. Al Amin <48113312+alaminhossain171@users.noreply.github.com>
2022-05-29 10:50:13 +0600

    initial commit

---
+++ src/server/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCfYVDGoIR3-fU4yQrguq7d1EyDJgv9BxE", // Add API Key
+  databaseURL:"https://livechatdatabase-default-rtdb.asia-southeast1.firebasedatabase.app" // Add databaseURL
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase;
+
+var firepadRef = firebase.database().ref();
+
+export const userName = prompt("What's your name?");
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
+
+if (roomId) {
+  firepadRef = firepadRef.child(roomId);
+} else {
+  firepadRef = firepadRef.push();
+  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
+}
+
+export default firepadRef;
